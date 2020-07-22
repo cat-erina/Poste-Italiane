cfb74d5030e3991860ce4a0e0cff6de0fcaaf3d9
cat-erina <56516698+cat-erina@users.noreply.github.com>
2020-07-22 12:30:26 +0200

    Add files via upload

---
+++ blabliblu.py
@@ -0,0 +1,19 @@
+import os
+
+path=os.listdir(os.getcwd())
+print(path)
+for elem in path:
+	f=open(elem,"r")
+	l=f.readlines()
+	for line in range(len(l)):
+		l[line] = l[line].replace(',', '.')
+
+		if "<name>" in l[line]:
+			l[line]=l[line].replace("2", "small villas")
+        	l[line]=l[line].replace("1", "large block building")
+
+			l[line]=l[line]+"        <pose></pose>\n        <truncated>0</truncated>\n        <difficult>0</difficult>\n"
+	f.close()
+	f=open(elem."w")
+	f.writelines(l)
+	f.close()
